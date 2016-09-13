b3b3c6042c818c1d7feb994338f39bdc92a5a651
Matthieu ARENE <marene@e1r1p6.42.fr>
2016-09-13 16:57:01 +0200

    ar handling ready to be implemented!

--- nm/srcs/get_magic.c
+++ nm/srcs/get_magic.c
@@ -6,7 +6,7 @@
 /*   By: marene <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/22 14:52:49 by marene            #+#    #+#             */
-/*   Updated: 2016/09/13 14:20:50 by marene           ###   ########.fr       */
+/*   Updated: 2016/09/13 16:56:29 by marene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,18 @@
 
 #include "ft_nm.h"
 
-static int		get_ar_size(char *ar_name)
+static int		get_size(char *ar_name)
 {
-	char	*str_size;
-
-	str_size = ft_strstr(ar_name, "/");
-	return ((str_size != NULL) ? ft_atoi(str_size + 1) : -1);
+	return (ft_atoi(ft_strstr(ar_name, "/") + 1));
 }
 
 int				get_magic(t_env *env)
 {
 	unsigned int		magic_nb;
 	void				*data = env->froot->content;
+	void				*tmp;
 	struct ar_hdr		ar_h;
-	int					ar_size;
-	int					obj_file_nb;
+	int					off;
 //	struct ranlib		ranl;
 
 	magic_nb = *(int *)env->froot->content;
@@ -53,11 +50,14 @@ int				get_magic(t_env *env)
 		printf("magic_lib\n");
 		ar_h = *(struct ar_hdr*)(env->froot->content + SARMAG);
 		data = env->froot->content + SARMAG + sizeof(struct ar_hdr);
-		ar_size = get_ar_size(ar_h.ar_name);
-		obj_file_nb = *(int*)(data + ar_size);
-		printf("ar_h.ar_name : [%s]\nobj_file_nb : %lu\n", ar_h.ar_name, obj_file_nb / sizeof(struct ranlib));
-		printf("ar_size : [%s] (%lx)\n", ar_h.ar_size, (data + ft_atoi(ar_h.ar_size)) - env->froot->content);
-		ar_h = *(struct ar_hdr*)(data + ft_atoi(ar_h.ar_size));
+		data += ft_atoi(ar_h.ar_size);
+		ar_h = *(struct ar_hdr*)(data);
+		off = get_size(ar_h.ar_name);
+		tmp = (data + sizeof(ar_h) + off);
+		if (*(unsigned int*)(tmp) == MH_MAGIC_64)
+			printf("IT FUCKING WORKED!\n");
+		env->froot->content = tmp;
+		return (get_magic(env));
 		return (NM_NOK);
 	}
 	else
